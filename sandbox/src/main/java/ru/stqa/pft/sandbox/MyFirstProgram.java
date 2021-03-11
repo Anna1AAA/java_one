8eef1ba1cba31b979d3462daeb8d45a5872f9359
Anna1AAA <solnishko601@mail.ru>
2021-03-11 17:44:36 +0300

    площадь квадрата с переменными

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,9 +7,15 @@ public class MyFirstProgram {
     hello("user");
     hello("Anna");
 
+    double i = 5;
+    System.out.println("Площадь квадрата со стороной " + i + " = " + area(i));
+
   }
+
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
-
-} 
+  public static double area(double l) {
+    return l*l;
+  }
+}
