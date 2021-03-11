766329424fedf2ffbc634aaba0a27ed1d7abef91
Anna1AAA <solnishko601@mail.ru>
2021-03-11 18:20:33 +0300

    созданы классы и объекты Квадрат и Прямоугольник

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,22 +7,24 @@ public class MyFirstProgram {
     hello("user");
     hello("Anna");
 
-    double i = 5;
-    System.out.println("Площадь квадрата со стороной " + i + " = " + area(i));
+    Square s = new Square();
+    s.l = 5;
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    Rectangle r = new Rectangle();
+    r.a = 4;
+    r.b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
-  public static double area(double l){
-    return l*l;
+  public static double area(Square s){
+    return s.l*s.l;
   }
-  public static double area(double a, double b) {
-    return a*b;
+  public static double area(Rectangle r) {
+    return r.a * r.b;
   }
 }
