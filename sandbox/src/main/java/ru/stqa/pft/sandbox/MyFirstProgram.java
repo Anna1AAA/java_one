c4228752f4755df835ff03244adcb41fd60f0442
Anna1AAA <solnishko601@mail.ru>
2021-03-11 17:33:38 +0300

    функция hello с измененными значениями

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,13 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello();
+    hello("world");
+    hello("user");
+    hello("Anna");
+
   }
-  public static void hello(){
-    System.out.println("Hello, world!");
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 
 } 
