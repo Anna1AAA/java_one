eb93b3d35dd9d63820c916d176cbb29b8925f787
Anna1AAA <solnishko601@mail.ru>
2021-03-11 16:58:31 +0300

    строковые переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,10 +4,11 @@ public class MyFirstProgram {
 
   public static void main(String[] args)
   {
-    System.out.println("Hello, world!");
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
 
-    int l = 3;
-    int s = l*l;
+    double l = 3;
+    double s = l*l;
     System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 
